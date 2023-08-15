ebcdc65270e9d08ce6e8b88a1f58096b8d8f6151
AGTechSolutions <142107015+AGTechSolutions@users.noreply.github.com>
2023-08-15 21:47:29 +0530

    Add files via upload

---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Videos from'./videos';
+
+function App(){
+return(<div>
+
+    <Videos/>
+   
+   </div>)
+
+}
+export default App;
---
+++ src/components/videos.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Videos()
+{
+    return(<div>
+       <h1 id="videosh1">Videos</h1>
+       <div className="divstlogo">
+       <img id="stlogo" src="stlogo.jpg"/>
+       </div>
+       <div className="divmsg">
+       
+       <h2 id="msg">Stay tune With us for more updates !</h2>
+       
+       </div>
+    </div>)
+}
+export default Videos;
